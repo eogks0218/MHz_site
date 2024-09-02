335260d2b6e65c386e35ab01e57bc8453f4298d1
eogks1660 <eogks1660@gmail.com>
2024-09-02 15:04:14 +0900

    0902-1

---
+++ src/components/ServiceViral/Process.js
@@ -0,0 +1,81 @@
+import { useEffect } from "react";
+import "../../scss/ServiceViral/Process.scss";
+
+export default function Process(){
+
+    useEffect(()=>{
+        const flags = document.querySelectorAll(".flag");
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('in-view');
+                } else {
+                    entry.target.classList.remove('in-view');
+                }
+            });
+        });
+
+        flags.forEach(flag => observer.observe(flag));
+
+        return() => {
+            flags.forEach(flag => observer.unobserve(flag));
+        }
+    }, [])
+
+    return(
+        <>
+            <ul class="timeline">
+                <li>
+                    <div class="direction-r">
+                        <div class="flag-wrapper">
+                            <span class="flag">제품 URL 전달 및 행사 일정 요청 시 가이드 양식 전달</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-l">
+                        <div class="flag-wrapper">
+                            <span class="flag">적절한 카페 / 커뮤니티 진입 여부 상담 후 선정</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-r">
+                        <div class="flag-wrapper">
+                            <span class="flag">원고 작성</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-l">
+                        <div class="flag-wrapper">
+                            <span class="flag">원고 컨펌 요청 후 최종 발행</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-r">
+                        <div class="flag-wrapper">
+                            <span class="flag">발행 전 해당 행사 구성 및 할인가 재검토</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-l">
+                        <div class="flag-wrapper">
+                            <span class="flag">발행 후 링크 전달 및 실시간 피드백(반응 대응)</span>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="direction-r">
+                        <div class="flag-wrapper">
+                            <span class="flag">행사 이후 상품에 대한 개선점 피드백</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </>
+    )
+}
--- src/components/pages/ServiceViralPage.js
+++ src/components/pages/ServiceViralPage.js
@@ -4,10 +4,8 @@ import FooterBox from "../common/FooterBox.js"
 import DownBtn from "../common/DownBtn.js"
 import { useState, useEffect } from "react"
 import "../../scss/pages/ServiceViralPage.scss"
-import { GoThumbsup } from "react-icons/go";
-import { TfiWrite } from "react-icons/tfi";
-import { FcOvertime } from "react-icons/fc";
-import { VscFeedback } from "react-icons/vsc";
+import Special from "../ServiceViral/Special.js"
+import Process from "../ServiceViral/Process.js"
 
 export default function ServiceViralPage(){
 
@@ -53,44 +51,17 @@ export default function ServiceViralPage(){
                         <h1>메가헤르츠만의 특별함</h1>
                     </div>
                     <div className="CV-description-two">
-                        <div className="specialBox">
-                            <div className="specialTitle">
-                                <GoThumbsup />
-                                <p>품질 높은 발행 계정</p>
-                            </div>
-                            <div className="specialContent">
-                                <p>공장처럼 찍어내는 계정으로 작업하게 되시면 효율이 나올 수가 없습니다.</p>
-                            </div>
-                        </div>
-                        <div className="specialBox">
-                            <div className="specialTitle">
-                                <TfiWrite />
-                                <p>원고 작성 능력</p>
-                            </div>
-                            <div className="specialContent">
-                                <p>제목, 본문, 말투부터 마침표 하나까지 신경 쓰는 디테일로 카페, 커뮤니티 특성에 맞춰 정확히 맞춤된 원고로 진입해야 합니다.</p>
-                            </div>
-                        </div>
-                        <div className="specialBox">
-                            <div className="specialTitle">
-                                <FcOvertime />
-                                <p>당일 게시,<br />자정, 주말 작업 가능</p>
-                            </div>
-                            <div className="specialContent">
-                                <p>A부터 Z까지 직접 진행하며 신속성과 정확성 그리고 정확한 타켓팅을 목표로 합니다.</p>
-                            </div>
-                        </div>
-                        <div className="specialBox">
-                            <div className="specialTitle">
-                                <VscFeedback />
-                                <p>즉각적인 1:1 피드백</p>
-                            </div>
-                            <div className="specialContent">
-                                <p>다양한 돌발 상황들 또한 완벽 대응 가능하며 게시 전, 게시 후 밀착 피드백이 가능합니다.<br/>즉각 반응이 나오는 곳임으로 피드백 속도는 생명입니다.</p>
-                            </div>
-                        </div>
+                        <Special />
                     </div>
                     <DownBtn position={2}/>
+                </div>
+                <div className="CV-content-three">
+                    <div className="CV-title-three">
+                        <h1>PROCESS</h1>
+                    </div>
+                    <div className="CV-description-three">
+                        <Process />
+                    </div>
                 </div>
             </div>
             <FooterBox />
