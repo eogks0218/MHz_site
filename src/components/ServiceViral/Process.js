25b06552ec06b89fa469cf5bd4a89f997933842a
eogks1660 <eogks1660@gmail.com>
2024-09-11 18:50:43 +0900

    0911-1

--- src/components/ServiceViral/Process.js
+++ src/components/ServiceViral/Process.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import "../../scss/ServiceViral/Process.scss";
+import { FaSquareCaretDown } from "react-icons/fa6";
 
 export default function Process(){
 
-    useEffect(()=>{
-        const flags = document.querySelectorAll(".flag");
+    useEffect(() => {
+        const CVs = document.querySelectorAll(".process-card, .process-svg" )
 
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
@@ -13,76 +14,65 @@ export default function Process(){
                 } else {
                     entry.target.classList.remove('in-view');
                 }
-            });
+            })
         });
 
-        flags.forEach(flag => observer.observe(flag));
-
+        CVs.forEach(entry => observer.observe(entry));
         return() => {
-            flags.forEach(flag => observer.unobserve(flag));
+            CVs.forEach(entry => observer.unobserve(entry));
         }
     }, [])
 
     return(
         <>
-            <ul class="timeline">
-                <li>
-                    <div class="direction-r">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">제품 URL 전달 및 행사 일정 요청 시 가이드 양식 전달</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-l">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">적절한 카페 / 커뮤니티 진입 여부 상담 후 선정</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-r">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">원고 작성</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-l">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">원고 컨펌 요청 후 최종 발행</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-r">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">발행 전 해당 행사 구성 및 할인가 재검토</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-l">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">발행 후 링크 전달 및 실시간 피드백(반응 대응)</span>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="direction-r">
-                        <div class="flag-wrapper">
-                            <div className="flag-image" />
-                            <span class="flag">행사 이후 상품에 대한 개선점 피드백</span>
-                        </div>
-                    </div>
-                </li>
-            </ul>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>제품 URL 전달 및 행사 일정 요청 시 가이드 양식 전달</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>적절한 카페 / 커뮤니티 진입 여부 상담 후 선정</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>원고 작성</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>원고 컨펌 요청 후 최종 발행</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>발행 전 해당 행사 구성 및 할인가 재검토</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>발행 후 링크 전달 및 실시간 피드백(반응 대응)</h3>
+                </div>
+            </div>
+            <FaSquareCaretDown className="process-svg"/>
+            <div className="process-card">
+                <div className="process-card-img"></div>
+                <div className="process-card-description">
+                    <h3>행사 이후 상품에 대한 개선점 피드백</h3>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/pages/ServiceViralPage.js
+++ src/components/pages/ServiceViralPage.js
@@ -16,7 +16,7 @@ export default function ServiceViralPage(){
     }, [])
 
     useEffect(() => {
-        const CVs = document.querySelectorAll(".CV-title-one, .CV-description-one, .CV-title-two, .CV-description-two, .CV-title-three, .CV-description-three")
+        const CVs = document.querySelectorAll(".CV-title-one, .CV-description-one, .CV-title-two, .CV-description-two, .CV-title-three, .CV-description-three, .CV-why-title h1, .CV-why-item" )
 
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
@@ -41,23 +41,45 @@ export default function ServiceViralPage(){
             <div className="ServiceCV-container">
                 <div className="CV-content-one">
                     <div className="CV-title-one">
-                        <h1>바이럴 마케팅</h1>
-                        <h5>메가헤르츠는 온라인 광고에 최적화된 통합 마케팅을 제공합니다.</h5>
-                    </div>
-                    <div className="CV-description-one">
-                        <div className="CV-description-one-image" />
-                        <div className="CV-description-one-content">
-                            <h4>바이럴 마케팅이 필요한 이유</h4>
-                            <p>
-                                사람이 모이는 곳에 기회가 있습니다.<br />
-                                이런 곳에 노출이 되어야 긍정적인 여론 형성을 통해<br />
-                                브랜드의 긍정적인 인지도 효과 및 매출 증대를 가져옵니다.<br /><br />
-                                기업의 브랜드 및 제품의 입소문이 가장 대규모로 전파 할 수 있는 공간은<br />
-                                가장 활성화된 대형 카페, 커뮤니티 공간에서의 활동입니다.
-                            </p>
-                        </div>
+                        <h1>효과적인 바이럴 마케팅, 메가헤르츠가 해결해 드립니다.</h1>
+                        <h5>바이럴 마케팅에 대한 모든 고민, 메가헤르츠에 맡겨주세요.</h5>
                     </div>
                     <DownBtn position={1}/>
+                </div>
+                <div className="CV-content-why">
+                    <div className="CV-why-title">
+                        <h1>왜 바이럴 마케팅이 필요할까요?</h1>
+                    </div>
+                    <div className="CV-why-list">
+                        <div className="CV-why-item">
+                            <div className="why-item-img"></div>
+                            <div className="why-item-description">
+                                <h3>입소문의 힘, 바이럴 마케팅</h3>
+                                <p>사람들은 자신이 신뢰하는 사람이나 커뮤니티의 추천을 더욱 신뢰합니다. 바이럴 마케팅은 이러한 입소문 효과를 통해 브랜드에 대한 긍정적인 인식을 확산시키고, 자연스러운 구매로 이어지도록 합니다.</p>
+                            </div>
+                        </div>
+                        <div className="CV-why-item">
+                            <div className="why-item-img"></div>
+                            <div className="why-item-description">
+                                <h3>잠재 고객과의 소통 창구</h3>
+                                <p>바이럴 마케팅은 단순히 정보를 전달하는 것을 넘어, 잠재 고객과 직접 소통하고 교감할 수 있는 기회를 제공합니다. 고객의 피드백을 통해 제품 또는 서비스를 개선하고, 더욱 강력한 브랜드 충성도를 구축할 수 있습니다.</p>
+                            </div>
+                        </div>
+                        <div className="CV-why-item">
+                            <div className="why-item-img"></div>
+                            <div className="why-item-description">
+                                <h3>경쟁 우위 확보</h3>
+                                <p>치열한 경쟁 속에서 차별화된 브랜드 이미지를 구축하고, 타겟 고객에게 강력한 인상을 심어주기 위해서는 바이럴 마케팅이 필수적입니다.</p>
+                            </div>
+                        </div>
+                        <div className="CV-why-item">
+                            <div className="why-item-img"></div>
+                            <div className="why-item-description">
+                                <h3>비용 효율적인 마케팅</h3>
+                                <p>기존의 광고 방식에 비해 상대적으로 적은 비용으로 많은 사람들에게 노출될 수 있습니다. 특히, 소셜 미디어를 활용한 바이럴 마케팅은 더욱 효과적이고 비용 효율적입니다.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="CV-content-two">
                     <div className="CV-title-two">
@@ -66,7 +88,6 @@ export default function ServiceViralPage(){
                     <div className="CV-description-two">
                         <Special />
                     </div>
-                    <DownBtn position={2}/>
                 </div>
                 <div className="CV-content-three">
                     <div className="CV-title-three">
